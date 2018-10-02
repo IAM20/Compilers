eec24d9c836654b41a2ecb673df57379f1e8ef36
201300264Lee <2013008264@hanyang.ac.kr>
2018-10-02 10:54:08 +0900

    Tab to space (just format change)

--- 01_project/src/globals.h
+++ 01_project/src/globals.h
@@ -2,39 +2,39 @@
 #define MAXRESERVED 12
 
 typedef enum {
-	/* Reserved words */
-	IF = 1, ELSE, WHILE, RETURN, INT, VOID,
-	
-	/* Discarded */
-	THEN, END, REPEAT, UNTIL, READ, WRITE,
-	/*************/
+  /* Reserved words */
+  IF = 1, ELSE, WHILE, RETURN, INT, VOID,
+  
+  /* Discarded */
+  THEN, END, REPEAT, UNTIL, READ, WRITE,
+  /*************/
 
-	/* Multicharacter inputs */
-	ID, NUM,
-	
-	/* Special symbols */
-	ASSIGN,		// =
-	EQ,				// ==
-	NE,				// !=
-	LT,				// <
-	LE,				// <=
-	GT,				// >
-	GE,				// >=
-	PLUS,			// +
-	MINUS,		// -
-	TIMES,		// *
-	OVER,			// '/'
-	LPAREN,		// '('
-	RPAREN,		// ')'
-	LSQUARE,	// '[' 
-	RSQUARE,	// ']'
-	LCURLY,		// '{'
-	RCURLY,		// '}'
-	SEMI,			// ;
-	COMMA,		// ,
-	LCOMMENT,	// /*
-	RCOMMENT,	// */
-	NEWLINE		// \n
+  /* Multicharacter inputs */
+  ID, NUM,
+  
+  /* Special symbols */
+  ASSIGN,   // =
+  EQ,       // ==
+  NE,       // !=
+  LT,       // <
+  LE,       // <=
+  GT,       // >
+  GE,       // >=
+  PLUS,     // +
+  MINUS,    // -
+  TIMES,    // *
+  OVER,     // '/'
+  LPAREN,   // '('
+  RPAREN,   // ')'
+  LSQUARE,  // '[' 
+  RSQUARE,  // ']'
+  LCURLY,   // '{'
+  RCURLY,   // '}'
+  SEMI,     // ;
+  COMMA,    // ,
+  LCOMMENT, // /*
+  RCOMMENT, // */
+  NEWLINE   // \n
 } TokenType;
 
 extern FILE * source;
--- 01_project/src/util.c
+++ 01_project/src/util.c
@@ -6,105 +6,105 @@
 
 int 
 printToken(FILE * fp) {
-	int token;
-	int line = 0;
-	int tmp;
-	while(token = yylex()) {
-		switch(token) {
-		case IF:
-			printf("%d: reserved word: if\n", line);
-			break;
-		case ELSE:
-			printf("%d: reserved word: else\n", line);
-			break;
-		case WHILE:
-			printf("%d: reserved word: while\n", line);
-			break;
-		case RETURN:
-			printf("%d: reserved word: return\n", line);
-			break;
-		case INT:
-			printf("%d: reserved word: int\n", line);
-			break;
-		case VOID:
-			printf("%d: reserved word: void\n", line);
-			break;
-		case ID:
-			printf("%d: ID, name= %s\n", line, yytext);
-			break;
-		case NUM:
-			printf("%d: NUM, value= %d\n", line, atoi(yytext));
-			break;
+  int token;
+  int line = 0;
+  int tmp;
+  while(token = yylex()) {
+    switch(token) {
+    case IF:
+      printf("%d: reserved word: if\n", line);
+      break;
+    case ELSE:
+      printf("%d: reserved word: else\n", line);
+      break;
+    case WHILE:
+      printf("%d: reserved word: while\n", line);
+      break;
+    case RETURN:
+      printf("%d: reserved word: return\n", line);
+      break;
+    case INT:
+      printf("%d: reserved word: int\n", line);
+      break;
+    case VOID:
+      printf("%d: reserved word: void\n", line);
+      break;
+    case ID:
+      printf("%d: ID, name= %s\n", line, yytext);
+      break;
+    case NUM:
+      printf("%d: NUM, value= %d\n", line, atoi(yytext));
+      break;
 
-		/* Case : special  Symbols! */
-		case ASSIGN:
-			printf("%d: =\n", line);
-			break;
-		case EQ:
-			printf("%d: ==\n", line);
-			break;
-		case NE:
-			printf("%d: !=\n", line);
-			break;
-		case LT:
-			printf("%d: <\n", line);
-			break;
-		case LE:
-			printf("%d: <=\n", line);
-			break;
-		case GT:
-			printf("%d: >\n", line);
-			break;
-		case GE:
-			printf("%d: >=\n", line);
-			break;
-		case PLUS:
-			printf("%d: +\n", line);
-			break;
-		case MINUS:
-			printf("%d: -\n", line);
-			break;
-		case TIMES:
-			printf("%d: *\n", line);
-			break;
-		case OVER:
-			printf("%d: /\n", line);
-			break;
-		case LPAREN:
-			printf("%d: (\n", line);
-			break;
-		case RPAREN:
-			printf("%d: )\n", line);
-			break;
-		case LSQUARE:
-			printf("%d: [\n", line);
-			break;
-		case RSQUARE:
-			printf("%d: ]\n", line);
-			break;
-		case LCURLY:
-			printf("%d: {\n", line);
-			break;
-		case RCURLY:
-			printf("%d: }\n", line);
-			break;
-		case SEMI:
-			printf("%d: ;\n", line);
-			break;
-		case COMMA:
-			printf("%d: ,\n", line);
-			break;
-		case LCOMMENT:
-			while((tmp = yylex()) != RCOMMENT) {
-				if(tmp == NEWLINE) line++;
-			}
-			break;
-		case NEWLINE:
-			line++;
-			break;
-		default:
-			printf("UNEXPECTED RETURN TOKEN NUMBER : %d\n", token);
-			return 1;
-		}
-	}
+    /* Case : special  Symbols! */
+    case ASSIGN:
+      printf("%d: =\n", line);
+      break;
+    case EQ:
+      printf("%d: ==\n", line);
+      break;
+    case NE:
+      printf("%d: !=\n", line);
+      break;
+    case LT:
+      printf("%d: <\n", line);
+      break;
+    case LE:
+      printf("%d: <=\n", line);
+      break;
+    case GT:
+      printf("%d: >\n", line);
+      break;
+    case GE:
+      printf("%d: >=\n", line);
+      break;
+    case PLUS:
+      printf("%d: +\n", line);
+      break;
+    case MINUS:
+      printf("%d: -\n", line);
+      break;
+    case TIMES:
+      printf("%d: *\n", line);
+      break;
+    case OVER:
+      printf("%d: /\n", line);
+      break;
+    case LPAREN:
+      printf("%d: (\n", line);
+      break;
+    case RPAREN:
+      printf("%d: )\n", line);
+      break;
+    case LSQUARE:
+      printf("%d: [\n", line);
+      break;
+    case RSQUARE:
+      printf("%d: ]\n", line);
+      break;
+    case LCURLY:
+      printf("%d: {\n", line);
+      break;
+    case RCURLY:
+      printf("%d: }\n", line);
+      break;
+    case SEMI:
+      printf("%d: ;\n", line);
+      break;
+    case COMMA:
+      printf("%d: ,\n", line);
+      break;
+    case LCOMMENT:
+      while((tmp = yylex()) != RCOMMENT) {
+        if(tmp == NEWLINE) line++;
+      }
+      break;
+    case NEWLINE:
+      line++;
+      break;
+    default:
+      printf("UNEXPECTED RETURN TOKEN NUMBER : %d\n", token);
+      return 1;
+    }
+  }
 }
