1c38c4cbe8b63dc762c6a1d34e491eb675f531e3
201300264Lee <2013008264@hanyang.ac.kr>
2018-10-03 23:52:14 +0900

    tests shell script modified

--- 01_project/using-lex/test.sh
+++ 01_project/using-lex/test.sh
@@ -1,22 +1,22 @@
 #! /bin/bash
 
-./bin/cminus_flex ./tests/gcd.cm > gcd-test.log
+./bin/cminus_flex ../tests/gcd.cm > gcd-test.log
 echo "Progress 1/5"
 cat gcd-test.log
 sleep .5
-./bin/cminus_flex ./tests/fact.cm > fact-test.log
+./bin/cminus_flex ../tests/fact.cm > fact-test.log
 echo "Progress 2/5"
 cat fact-test.log
 sleep .5
-./bin/cminus_flex ./tests/fault.cm > fault-test.log
+./bin/cminus_flex ../tests/fault.cm > fault-test.log
 echo "Progress 3/5"
 cat fault-test.log
 sleep .5
-./bin/cminus_flex ./tests/all-comment.cm > all-comment-test.log
+./bin/cminus_flex ../tests/all-comment.cm > all-comment-test.log
 echo "Progress 4/5"
 cat all-comment-test.log
 sleep .5
-./bin/cminus_flex ./tests/hello-cminus.cm > hello-cminus-test.log
+./bin/cminus_flex ../tests/hello-cminus.cm > hello-cminus-test.log
 cat hello-cminus-test.log
 echo "Progress 5/5"
 sleep .5
